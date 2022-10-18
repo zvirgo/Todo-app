26094b92255a9db2391ced21353f75ba1c7ea6ec
zeinali <zeinali.b69@gmail.com>
2022-10-18 14:38:48 +0330

    complete Todos & Todo component

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import Todo from './Todo'
 import Next7Days from './Next7Days'
+import { todos } from '../constants'
 
 const Todos = () => {
+  const selectedProject = "today"
   return (
     <div className='Todos'>
-      <Todo />
-      <Next7Days />
+      <div className='selected-project'>
+        {selectedProject}
+      </div>
+      <div className="todos">
+        {
+          selectedProject === "next 7 days" ?
+            <Next7Days todos={todos} />
+            :
+            todos.map(todo =>
+              <Todo todo={todo} key={todo.id} />
+            )
+        }
+      </div>
     </div>
   )
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -4,3 +4,25 @@ export const projects = [
     { id: 2, name: "Work", numOfTodos: 1 },
     { id: 3, name: "Other", numOfTodos: 2 }
 ]
+export const todos = [
+    {
+        id: 'd54sd4',
+        text: "Go for a run",
+        time: "10:00 AM",
+        date: "06/03/2021",
+        day: "6",
+        checked: false,
+        color: '#000000',
+        project: 'personal'
+    },
+    {
+        id: 'd54fdf',
+        text: "Meeting",
+        time: "09:00 AM",
+        date: "08/03/2021",
+        day: "1",
+        checked: true,
+        color: '#00ff00',
+        project: 'work'
+    }
+]
