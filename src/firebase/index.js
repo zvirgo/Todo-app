52b587ee04523f652f42ccd237564c92a98e4433
zeinali <zeinali.b69@gmail.com>
2022-10-19 14:53:33 +0330

    Add firebase setting

---
+++ src/firebase/index.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app"
+import "firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCAefyA5o8c_cBGYUBwWip8MwhH2TVOcXU",
+    authDomain: "todo-react-app-5df2a.firebaseapp.com",
+    projectId: "todo-react-app-5df2a",
+    storageBucket: "todo-react-app-5df2a.appspot.com",
+    messagingSenderId: "20648455723",
+    appId: "1:20648455723:web:da12879f210f8d478b046e"
+};
+firebase.initializeApp(firebaseConfig);
+
+export default firebase
